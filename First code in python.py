31a106c3491ebed22fb053a540ea977a705e825d
Sri Harish Popuri <sriharish.popuri1@gmail.com>
2018-04-16 17:43:37 -0500

    Update First code in python.py

--- First code in python.py
+++ First code in python.py
@@ -180,3 +180,151 @@ subx2
 subx2[0,0] = 16
 subx2
 x2
+
+subx2 = x2[:2,:2].copy()
+subx2
+subx2[0,0] = 3
+subx2
+x2
+
+
+import numpy as np
+
+grid = np.arange(1,10)
+grid
+type(grid)
+grid = grid.reshape((3,3))
+grid
+type(grid)
+
+x = np.array([1,2,3])
+x
+x.reshape((3,1))
+
+x
+x[:,np.newaxis]
+
+x = np.array([1,2,3])
+y = np.array([3,2,1])
+np.concatenate((x,y))
+np.concatenate?
+
+x
+y
+np.concatenate((x[:,np.newaxis],y[:,np.newaxis]))
+np.vstack((x,y))
+np.shape(np.vstack((x,y)))
+np.hstack((x,y))
+np.shape(np.hstack((x,y)))
+
+np.vstack((grid,x))
+grid
+np.concatenate((grid,grid))
+
+np.shape(grid)
+np.shape(np.concatenate((x,y)))
+np.ndim(np.concatenate((x,y)))
+
+np.shape(np.concatenate((grid,grid)))
+np.concatenate((grid,grid))
+np.concatenate((grid,grid),axis = 1)
+
+x = [1, 2, 3, 99, 99, 3, 2, 1]
+x1, x2, x3 = np.split(x, [3, 5])
+print(x1, x2, x3)
+np.split?
+x
+y1, y2 = np.split(x,[2])
+y1
+y2
+
+grid = np.arange(16).reshape((4,4))
+grid
+upper, lower = np.vsplit(grid,[2])
+upper
+lower
+
+left, right = np.hsplit(grid,[2])
+left
+right
+
+np.random.seed(1601)
+np.random.randint(6,size = 10)
+s = range(10)
+s
+
+ def compute_reciprocals(values):
+   output = np.empty(len(values))
+   for i in range(len(values)):
+     output[i] = 1.0 / values[i]
+  return output
+
+np.random.seed(0)    
+values = np.random.randint(1, 10, size=5)
+values
+compute_reciprocals(values)
+
+np.empty(5)
+np.empty(10)
+
+big_array = np.random.randint(1, 100, size=1000000)
+%timeit compute_reciprocals(big_array)
+
+np.arange(5)/np.arange(1,6)
+np.arange(5)*np.arange(1,6)
+np.arange(5)+np.arange(1,6)
+
+x = np.arange(9).reshape((3,3))
+x
+2**x # 2 power x
+x**x # x power x
+
+x
+x+2
+x-2
+x*4
+x/5
+x//2
+x%2
+x/2
+-x
+
+(0.5*x + 1)**2
+
+np.add(x,2)
+
+np.arange(-2,4)
+np.abs(np.arange(-2,4))
+
+z = np.array([3 - 4j, 4 - 3j, 2 + 0j, 0 + 1j])    
+z
+np.abs(z)
+
+np.pi
+theta = np.linspace(0,np.pi,5)
+theta
+
+np.sin(theta)
+np.cos(theta)
+np.tan(theta)
+
+np.arccos(-1)
+
+x = np.array([1,2,3])
+x
+np.exp(x)
+np.exp2(x)
+np.power(3,x)
+np.power(x,3)
+
+np.log(x)
+np.log2(x)
+np.log10(x)
+np.log1p(1) # more precise values when x is small
+
+
+from scipy import special as sp
+sp.gamma(5)
+sp.gamma(2)
+sp.gamma(6)
+
