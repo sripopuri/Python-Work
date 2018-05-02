4bcdd1489b4bb412c4d6deb7c303df83ed14d790
Sri Harish Popuri <sriharish.popuri1@gmail.com>
2018-05-02 01:06:47 -0500

    Update Pandas.py

--- Pandas.py
+++ Pandas.py
@@ -92,7 +92,68 @@ ind[5]
 ind[::2]
 ind[::1]
 
+print(ind.size,ind.shape,ind.ndim,ind.dtype)
 
+indA = pd.Index([1, 3, 5, 7, 9])
+indB = pd.Index([2, 3, 5, 7, 11])
+indA
+
+indA & indB
+indA | indB
+indA ^ indB
+
+import pandas as pd
+data = pd.Series([0.25,0.5,0.75,1.0],index =['a','b','c','d'])
+data
+data['b']
+'c' in data
+'z' in data
+data.keys()
+data.values
+data.index
+list(data.items())
+data['e'] = 01.25
+data
+data['a':'c']
+data.values[1:3]
+
+data1 = pd.Series([1,2,3,4],index = [1,2,3,4])
+data1[1:3]
+data1
+
+data
+data[(data > 0.1) & (data < 0.6)]
+
+data = pd.Series(['a','b','c'],index=[1,3,5])
+data
+data[1]
+data[0:2]
+data.loc[1]
+data.loc[1:3]
+data.iloc[1:3]
+data
+
+area = pd.Series({'California': 423967, 'Texas': 695662,
+                  'New York': 141297, 'Florida': 170312,
+                  'Illinois': 149995})
+pop = pd.Series({'California': 38332521, 'Texas': 26448193,
+                 'New York': 19651127, 'Florida': 19552860,
+                 'Illinois': 12882135})
+data = pd.DataFrame({'area':area,'pop':pop})
+data
+data['area']
+data.pop
+data['pop']
+data['density'] = data['pop']/data['area']
+data
+
+data.values
+data.T
+
+data.values[0]
+data
+
+data.iloc[0:3,0:2]
 
 
 
